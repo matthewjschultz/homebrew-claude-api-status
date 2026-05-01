40534800f30fb63368b2a93472f455b8222ae0a8
Matthew J. Schultz <45434369+matthewjschultz@users.noreply.github.com>
2026-04-30 20:11:39 -0400

    claudeapistatus: update to 1.2.0

--- Casks/claude-api-status.rb
+++ Casks/claude-api-status.rb
@@ -1,6 +1,6 @@
 cask "claude-api-status" do
-  version "1.1.0"
-  sha256 "eb75eed5533817ad01f1cdb65a829f1ab5bc2309c9acd1f47cbe4bfbd8c7634d"
+  version "1.2.0"
+  sha256 "e0a78d5a497c611fe4358404dc652ceb3a6f25e137dad255ac56138c4aaecbf3"
 
   url "https://github.com/matthewjschultz/homebrew-claude-api-status/releases/download/v#{version}/ClaudeAPIStatus-#{version}.dmg"
   name "Claude API Status"
